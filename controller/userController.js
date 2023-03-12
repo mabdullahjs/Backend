0061022f0dce69f99b4b0d053fdc1ec0ca81fe9f
mabdullah <mabdullah2037@gmail.com>
2023-03-12 12:35:44 +0500

    password in database with hashing completed

--- controller/userController.js
+++ controller/userController.js
@@ -2,11 +2,17 @@ const User = require('../models/userModel');
 
 //login user
 const loginUser = async (req, res) => {
-    res.json({mssg:'login User'});
+    res.json({ mssg: 'login User' });
 }
 
 const signupUser = async (req, res) => {
-    res.json({mssg:'signup User'});
+    const { email, password } = req.body;
+    try {
+        const user = await User.signup(email, password);
+        res.json({ email, user });
+    } catch(error) {
+        res.json({ error: error.message })
+    }
 }
 
-module.exports = {loginUser , signupUser};
+module.exports = { loginUser, signupUser };
