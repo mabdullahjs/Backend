df0a9cc07537b1cd800b87104451ddb2f3b68809
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-03-13 14:30:14 +0500

    jwt token added

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,12 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+
+//creating jwt token
+
+const createToken = (_id)=>{
+    return jwt.sign({_id} , process.env.SECRET , {expiresIn:'3d'})
+}
 
 //login user
 const loginUser = async (req, res) => {
@@ -9,7 +17,10 @@ const signupUser = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.signup(email, password);
-        res.json({ email, user });
+
+        // create token
+        const token = createToken(user._id)
+        res.json({ email, token });
     } catch(error) {
         res.json({ error: error.message })
     }
